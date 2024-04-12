3868d769752348b4f9ed6c4f3b929970729779ab
AlejandroRJBatuecas <alejandroramiro.jimenez@alu.uclm.es>
2024-04-12 13:29:39 +0200

    Aplicación de técnicas de selección de características para optimizar el modelo

--- ml_models/ml_utils.py
+++ ml_models/ml_utils.py
@@ -1,43 +1,35 @@
 import utils as utils
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.calibration import cross_val_predict
-from sklearn.metrics import accuracy_score, f1_score, classification_report, multilabel_confusion_matrix
-from sklearn.model_selection import cross_val_score
-
-# Limpiar números decimales
-def clean_decimal_numbers(number_str):
-    # Comprobar si es un float
-    if isinstance(number_str, float):
-        return number_str
-    else:
-        # Separar la parte entera y decimal
-        partes = number_str.split('.', 1)
-        # Eliminar puntos adicionales de la parte entera
-        parte_entera_sin_puntos = partes[0].replace(".", "")
-        # Reemplazar puntos en la parte decimal
-        parte_decimal_con_punto = partes[1].replace(".", "")
-        # Reconstruir el número con un solo punto
-        numero_str_sin_puntos_extra = parte_entera_sin_puntos + "." + parte_decimal_con_punto
-        # Convertir a número decimal
-        numero_decimal = float(numero_str_sin_puntos_extra)
-        return numero_decimal
-
-# Limpiar los datos corruptos de las columnas - m.AvgDens - m.AvgCNOT - m.AvgToff
-def clean_outliers(data):
-    for row in range(len(data["m.AvgDens"])):
-        data.iat[row, data.columns.get_loc('m.AvgDens')] = clean_decimal_numbers(data.iat[row, data.columns.get_loc('m.AvgDens')])
-        data.iat[row, data.columns.get_loc('m.AvgCNOT')] = clean_decimal_numbers(data.iat[row, data.columns.get_loc('m.AvgCNOT')])
-        data.iat[row, data.columns.get_loc('m.AvgToff')] = clean_decimal_numbers(data.iat[row, data.columns.get_loc('m.AvgToff')])
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, multilabel_confusion_matrix
+from sklearn.feature_selection import SelectKBest, mutual_info_regression, SequentialFeatureSelector, RFE
+from sklearn.linear_model import LinearRegression, LassoCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
 
 # Mostrar la estructura de los datos
-def show_data_structure(data):
+def show_data_structure(data, data_values, data_labels, train_set_values, test_set_values, train_set_labels, test_set_labels):
     # Mostrar los 5 primeros registros
     print(data.head())
     # Mostrar la estructura de los datos
     data.info()
     # Mostrar las estadísticas de los datos numéricos
     print(data.describe())
+    # Mostrar el total de patrones detectados y tamaño de los dataframes
+    print("\nTotal", data_labels["p.initialization"].value_counts())
+    print("Total", data_labels["p.superposition"].value_counts())
+    print("Total", data_labels["p.oracle"].value_counts())
+
+    print("\nFormato antes del train-test split:")
+    print("Formato de los valores:", data_values.shape)
+    print("Formato de las etiquetas:", data_labels.shape)
+
+    # Mostrar el tamaño de los conjuntos de entrenamiento y de prueba
+    print("\nFormato después del train-test split:")
+    print("Formato de los valores de entrenamiento:", train_set_values.shape)
+    print("Formato de los valores de prueba:", test_set_values.shape)
+    print("Formato de las etiquetas de entrenamiento:", train_set_labels.shape)
+    print("Formato de las etiquetas de prueba:", test_set_labels.shape)
 
 # Crear histograma de los atributos
 def create_data_histogram(data):
@@ -46,24 +38,103 @@ def create_data_histogram(data):
     plt.show()
 
 # Ver la correlación entre los datos
-def get_correlation_matrix(data):
+def get_correlation_matrix(data, min_correlation_value, patterns_list):
     # Calcular correlación
-    correlation_matrix = data.corr()
+    correlation_matrix = data.corr().abs()
 
     # Plot
     plt.figure(figsize=(8, 6))
     plt.imshow(correlation_matrix, cmap='coolwarm', interpolation='nearest')
     plt.colorbar(label='Correlation')
     plt.title('Matriz de correlación')
-    plt.xticks(range(len(correlation_matrix)), correlation_matrix.columns, rotation=45)
+    plt.xticks(range(len(correlation_matrix)), correlation_matrix.columns, rotation=90)
     plt.yticks(range(len(correlation_matrix)), correlation_matrix.columns)
     plt.tight_layout()
 
     # Guardar la imagen
-    utils.save_fig('matriz_correlacion.png')
+    utils.save_fig('correlation_matrix')
 
-    # Mostrar el gráfico
-    plt.show()
+    # Seleccionar las características con alta correlación con las variables objetivo
+    print(f"\nCaracterísticas con alta correlación (>{min_correlation_value}):")
+    for pattern in patterns_list:
+        # Obtener las mejores características para cada patrón
+        high_correlation_features = correlation_matrix[pattern][correlation_matrix[pattern] > min_correlation_value].index.tolist()
+        if pattern in high_correlation_features:
+            high_correlation_features.remove(pattern)  # Eliminar la variable objetivo de la lista
+        print(f"{pattern}:", high_correlation_features)
+
+# Obtener la información mutua respecto a las variables objetivo
+# Seleccionar las mejores características con Búsqueda hacia adelante
+def get_best_features(data_values, data_labels, train_set_values, train_set_labels, min_importance_value):
+    best_features = []
+
+    print("\nCaracterísticas seleccionadas por patrón:")
+    for label in data_labels:
+        print("\nPatrón :", label)
+
+        # Información mutua
+        pattern = data_labels[label]
+        # # Aplicar la información mutua para seleccionar las 5 mejores características
+        selector = SelectKBest(mutual_info_regression, k=5)
+        selector.fit_transform(data_values, pattern)
+        index_list = selector.get_support(indices=True)
+        features_names = [data_values.columns[index] for index in index_list]
+        print(f"Información mutua:", features_names)
+
+        # Búsqueda hacia adelante
+        # # Inicializar el modelo de regresión lineal
+        lineal_regressor = LinearRegression()
+        # # Aplicar la Búsqueda hacia adelante
+        sequential_feature_selector = SequentialFeatureSelector(lineal_regressor, n_features_to_select=5, direction='forward')
+        sequential_feature_selector.fit(train_set_values, train_set_labels[label])
+        # Identificar las características seleccionadas
+        index_list = sequential_feature_selector.get_support(indices=True)
+        features_names = [data_values.columns[index] for index in index_list]
+        print("Búsqueda hacia adelante:", features_names)
+
+        # Eliminación Recursiva de Características (RFE)
+        # # Inicializar el modelo de regresión lineal
+        lineal_regressor = LinearRegression()
+        # # Aplicar RFE
+        selector = RFE(lineal_regressor, n_features_to_select=5)
+        selector = selector.fit(data_values, pattern)
+        # # Identificar las características seleccionadas
+        selected_features = [data_values.columns[index] for index in range(len(selector.support_)) if selector.support_[index]]
+        print("RFE:", selected_features)
+
+        # Método de regularización L1 - Lasso
+        # # Normalizar las características
+        if label != "p.entanglement": # Da fallo para este patrón, ya que no hay ningún registro que lo implemente
+            scaler = StandardScaler()
+            scaled_data_values = scaler.fit_transform(data_values)
+
+            # # Ajustar el modelo Lasso con validación cruzada para encontrar el mejor alpha
+            lasso = LassoCV(cv=5, max_iter=10000)
+            lasso.fit(scaled_data_values, pattern)
+
+            # # Identificar las características seleccionadas (coeficientes diferentes de cero)
+            index_list = np.where(lasso.coef_ != 0)[0]
+            selected_features = [data_values.columns[index] for index in index_list]
+            print("Lasso:", selected_features)
+
+        # Evaluación de la importancia de las características - Random Forest
+        # Ajustar el modelo de Random Forest
+        forest = RandomForestRegressor(n_estimators=100)
+        forest.fit(data_values, pattern)
+
+        # Obtener la importancia de las características
+        importances = forest.feature_importances_
+        indices = np.argsort(importances)[::-1]
+
+        # Imprimir la importancia de las características
+        print("Importancia de las características:")
+        for f in range(data_values.shape[1]):
+            print(f"{data_values.columns[indices[f]]}: {importances[indices[f]]}")
+            # Añadir a la lista de mejores características si su importancia es mayor del 10%
+            if importances[indices[f]] > min_importance_value and data_values.columns[indices[f]] not in best_features:
+                best_features.append(data_values.columns[indices[f]])
+
+    return best_features
 
 # Obtener los datos sin etiquetas y las etiquetas
 def separate_data_and_labels(data_set, labels_list):
@@ -78,33 +149,27 @@ def separate_data_and_labels(data_set, labels_list):
     return data_set_values, data_set_labels
 
 # Evaluar el rendimiento del modelo
-def model_performance_data(test_set_labels, test_pred, knn_classifier, train_set_num, train_set_labels_np_matrix, train_set_labels):
-    # Transponer las listas colocando los datos en columnas
-    test_set_labels_np_matrix = np.c_[
-        test_set_labels['p.initialization'],
-        test_set_labels['p.superposition'],
-        test_set_labels['p.oracle'],
-        test_set_labels['p.entanglement']]
-    
-    # Evaluar la puntuación de exactitud
-    accuracy = accuracy_score(test_set_labels_np_matrix, test_pred)
-    print(f"Precisión del modelo: {round(accuracy*100, 2)}%")
+def model_performance_data(data_labels_np_matrix, predictions, patterns_list):
+    # Evaluar la exactitud
+    accuracy = accuracy_score(data_labels_np_matrix, predictions)
+    print(f"Exactitud: {round(accuracy*100, 2)}%")
 
-    # Evaluar la exactitud mediante evaluación cruzada
-    cross_val = cross_val_predict(knn_classifier, train_set_num, train_set_labels_np_matrix, cv=3)
-    #print("Validación cruzada: ", cross_val)
+    # Evaluar la precisión
+    precision = precision_score(data_labels_np_matrix, predictions, average="weighted", zero_division=np.nan)
+    print(f"Precisión: {round(precision*100, 2)}%")
+
+    # Evaluar la sensibilidad
+    recall = recall_score(data_labels_np_matrix, predictions, average="weighted", zero_division=np.nan)
+    print(f"Sensibilidad: {round(recall*100, 2)}%")
 
     # Calcular el f1 score
-    f1_score_value = f1_score(train_set_labels_np_matrix, cross_val, average="weighted", zero_division=np.nan)
-    print(f"F1 score: {round(f1_score_value*100, 2)}%")
+    f1 = f1_score(data_labels_np_matrix, predictions, average="weighted", zero_division=np.nan)
+    print(f"F1 score: {round(f1*100, 2)}%")
 
     # Imprimir el informe de clasificación
     print("Informe de clasificación:")
-    print(classification_report(test_set_labels_np_matrix, test_pred, zero_division=np.nan, target_names=list(train_set_labels.keys())))
-
-    # Calcular la matriz de confusión en el conjunto de prueba
-    conf_matrix = multilabel_confusion_matrix(test_set_labels_np_matrix, test_pred)
+    print(classification_report(data_labels_np_matrix, predictions, target_names=patterns_list, zero_division=np.nan))
 
     # Imprimir la matriz de confusión
     print("Matriz de Confusión:")
-    print(conf_matrix)
+    print(multilabel_confusion_matrix(data_labels_np_matrix, predictions))
--- ml_models/multilabel_classification.py
+++ ml_models/multilabel_classification.py
@@ -6,10 +6,14 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.calibration import cross_val_predict
 
 # Parámetros generales
-test_set_size = 0.3
-patterns_list = ["p.initialization", "p.superposition", "p.oracle", "p.entanglement"]
+test_set_size = 0.3 # Porcentaje de datos utilizados para el conjunto de prueba
+patterns_list = ["p.initialization", "p.superposition", "p.oracle", "p.entanglement"] # Lista de patrones a detectar
+min_correlation_value = 0.5 # Selecciona características con una correlación superior a este valor
+min_importance_value = 0.04 # Selecciona características con una importancia superior a este valor
+cv_value = 3 # Número de particiones realizadas en la validación cruzada. Por defecto = 5
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -17,73 +21,118 @@ patterns_list = ["p.initialization", "p.superposition", "p.oracle", "p.entanglem
 data = pd.read_csv(Path("../datasets/dataset_openqasm_qiskit.csv"), delimiter=";")
 test_data = pd.read_csv(Path("../datasets/prueba_cinco_dataset_openqasm_qiskit.csv"), delimiter=";")
 
-# Mostrar la estructura de los datos
-#mlutils.show_data_structure(data)
-#mlutils.create_data_histogram(data)
-
 # Limpiar las filas con algún dato nulo
 data = data.dropna()
 
+# Separar datos y etiquetas
+data_values = data.select_dtypes(include=[np.number])
+data_labels = data[patterns_list]
+
+# Obtener el conjunto de entrenamiento y de prueba
+train_set_values, test_set_values, train_set_labels, test_set_labels = train_test_split(data_values, data_labels, test_size=test_set_size, stratify=data_labels)
+
+# Mostrar la estructura de los datos
+mlutils.show_data_structure(data, data_values, data_labels, train_set_values, test_set_values, train_set_labels, test_set_labels)
+#mlutils.create_data_histogram(data)
+
 # Ver la correlación entre los datos
-data_num = data.select_dtypes(include=[np.number])
-#mlutils.get_correlation_matrix(data_num)
+data_vars = data.drop(["id", "language", "extension", "author", "name", "path", "circuit"], axis=1)
+# # Obtener la matriz de correlación
+mlutils.get_correlation_matrix(data_vars, min_correlation_value, patterns_list)
+# # Obtener las mejores características
+best_features = mlutils.get_best_features(data_values, data_labels, train_set_values, train_set_labels, min_importance_value)
 
-# Obtener el conjunto de prueba
-print("Nº de archivos: ", len(data))
-train_set, test_set = train_test_split(data, test_size=test_set_size)
-print("Tamaño del conjunto de entrenamiento: "+str(len(train_set))+" ("+str(round((len(train_set)/len(data))*100, 2))+"%)")
-print("Tamaño del conjunto de prueba: "+str(len(test_set))+" ("+str(round((len(test_set)/len(data))*100, 2))+"%)")
-
-# Preparar los datos para el algoritmo
-# Conjunto de entrenamiento
-train_set_values, train_set_labels = mlutils.separate_data_and_labels(train_set, patterns_list)
-# Conjunto de prueba
-test_set_values, test_set_labels = mlutils.separate_data_and_labels(test_set, patterns_list)
-
-# Obtener los valores numéricos de los datos
-train_set_num = train_set_values.select_dtypes(include=[np.number])
-test_set_num = test_set_values.select_dtypes(include=[np.number])
+# Obtener los datos de entrenamiento y prueba con las mejores métricas
+best_features_train_set_values = train_set_values[best_features]
+best_features_test_set_values = test_set_values[best_features]
 
 # Escalar los atributos
 scaler = StandardScaler()
-train_set_num = scaler.fit_transform(train_set_num)
-test_set_num = scaler.transform(test_set_num)
-
-# Transponer las listas colocando los datos en columnas
-train_set_labels_np_matrix = np.c_[
-    train_set_labels['p.initialization'],
-    train_set_labels['p.superposition'],
-    train_set_labels['p.oracle'],
-    train_set_labels['p.entanglement']]
+best_features_scaler = StandardScaler()
+# # Ajustar el escalador y tranformar los datos de entrenamiento
+train_set_values = scaler.fit_transform(train_set_values)
+# # Transformar los datos de prueba
+test_set_values = scaler.transform(test_set_values)
+# # Lo mismo para los datos mejorados
+best_features_train_set_values = best_features_scaler.fit_transform(best_features_train_set_values)
+best_features_test_set_values = best_features_scaler.transform(best_features_test_set_values)
 
 # Crear el clasificador multietiqueta
 knn_classifier = KNeighborsClassifier()
+best_features_knn_classifier = KNeighborsClassifier()
+
+# Transponer las listas colocando los datos en columnas para cada patrón
+train_set_labels_np_matrix = np.c_[tuple(train_set_labels[pattern] for pattern in patterns_list)]
 
 # Entrenar el clasificador
-knn_classifier.fit(train_set_num, train_set_labels_np_matrix)
-
-# Realizar predicciones en el conjunto de prueba
-test_pred = knn_classifier.predict(test_set_num)
+knn_classifier.fit(train_set_values, train_set_labels_np_matrix)
+best_features_knn_classifier.fit(best_features_train_set_values, train_set_labels_np_matrix)
 
 # Evaluar el rendimiento del modelo
-mlutils.model_performance_data(test_set_labels, test_pred, knn_classifier, train_set_num, train_set_labels_np_matrix, train_set_labels)
+# # Calcular las predicciones del clasificador mediante evaluación cruzada
+predictions = cross_val_predict(knn_classifier, train_set_values, train_set_labels_np_matrix, cv=cv_value)
 
+# # Mostrar las medidas de rendimiento
+print("\nRendimiento del modelo entrenado")
+mlutils.model_performance_data(train_set_labels_np_matrix, predictions, patterns_list)
+
+# Evaluar el rendimiento del modelo con las mejores métricas
+# # Calcular las predicciones del clasificador mediante evaluación cruzada
+predictions = cross_val_predict(best_features_knn_classifier, best_features_train_set_values, train_set_labels_np_matrix, cv=cv_value)
+
+# # Mostrar las medidas de rendimiento
+print(f"\nMétricas seleccionadas: {best_features}")
+print("Rendimiento del modelo mejorado")
+mlutils.model_performance_data(train_set_labels_np_matrix, predictions, patterns_list)
+
+# Realizar predicciones en el conjunto de prueba
+predictions = knn_classifier.predict(test_set_values)
+
+# Transponer las listas colocando los datos en columnas para cada patrón
+test_set_labels_np_matrix = np.c_[tuple(test_set_labels[pattern] for pattern in patterns_list)]
+
+# # Mostrar las medidas de rendimiento
+print("\nRendimiento del modelo con el conjunto de prueba")
+mlutils.model_performance_data(test_set_labels_np_matrix, predictions, patterns_list)
+
+# Realizar predicciones en el conjunto de prueba con las mejores métricas
+predictions = best_features_knn_classifier.predict(best_features_test_set_values)
+
+# # Mostrar las medidas de rendimiento
+print(f"\nMétricas seleccionadas: {best_features}")
+print("\nRendimiento del modelo mejorado con el conjunto de prueba")
+mlutils.model_performance_data(test_set_labels_np_matrix, predictions, patterns_list)
+
+'''
 # Preparar los datos de prueba
-# Eliminar las columnas con valores corruptos
-#test_set_data = test_data.drop(["m.AvgDens", "m.AvgCNOT", "m.AvgToff"], axis=1)
-# Obtener y escalar los datos numéricos
+# # Obtener los datos numéricos
 test_set_data_values = test_data.select_dtypes(include=[np.number])
-test_set_data_values = scaler.transform(test_set_data_values)
+# # Obtener los datos de prueba con las mejores métricas
+best_features_test_set_data_values = test_data[best_features]
 
-# Obtener predicciones de los datos de prueba con el modelo entrenado
+# Escalar los datos de prueba
+test_set_data_values = scaler.transform(test_set_data_values)
+best_features_test_set_data_values = best_features_scaler.transform(best_features_test_set_data_values)
+
+# Obtener predicciones y probabilidades de los datos de prueba con el modelo entrenado
 test_data_pred = knn_classifier.predict(test_set_data_values)
 predictions_proba = knn_classifier.predict_proba(test_set_data_values)
 
+# Realizar predicciones con los datos de prueba
+best_features_test_data_pred = best_features_knn_classifier.predict(best_features_test_set_data_values)
+best_features_predictions_proba = best_features_knn_classifier.predict_proba(best_features_test_set_data_values)
+
 # Imprimir los porcentajes de predicción
 for i in range(len(predictions_proba[0])):
-    print(f"Instancia {i+1}:")
+    print(f"\nInstancia {i+1}:")
     for j in range(len(predictions_proba)):
         if len(predictions_proba[j][i]) > 1 and (predictions_proba[j][i][1] > predictions_proba[j][i][0]):
             print(f" Cumple el patrón {patterns_list[j]} en un {round(predictions_proba[j][i][1]*100, 2)}%")
         else:
             print(f" No cumple el patrón {patterns_list[j]} en un {round(predictions_proba[j][i][0]*100, 2)}%")
+
+        if len(best_features_predictions_proba[j][i]) > 1 and (best_features_predictions_proba[j][i][1] > best_features_predictions_proba[j][i][0]):
+            print(f" Cumple el patrón {patterns_list[j]} en un {round(best_features_predictions_proba[j][i][1]*100, 2)}%")
+        else:
+            print(f" No cumple el patrón {patterns_list[j]} en un {round(best_features_predictions_proba[j][i][0]*100, 2)}%")
+'''
