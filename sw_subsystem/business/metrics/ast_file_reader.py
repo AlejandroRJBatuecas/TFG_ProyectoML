56bd9117b8f2cb1a1592684343beb59268f48a98
AlejandroRJB <alejandroramiro.jimenez@alu.uclm.es>
2024-07-11 11:55:13 +0200

    Corrección de issues de mantenibilidad en ast_file_reader

--- sw_subsystem/business/metrics/ast_file_reader.py
+++ sw_subsystem/business/metrics/ast_file_reader.py
@@ -30,52 +30,68 @@ class QiskitAnalyzer(ast.NodeVisitor):
 
         self.generic_visit(node)
 
+    # Establecer el circuito cuántico
+    def _set_quantum_circuit(self, var_name, node):
+        # Si el circuito no ha sido creado anteriormente
+        if var_name not in self.circuits:
+            circuit = QuantumCircuit()
+            # Añadir cada registro asignado al circuito
+            for arg in node.value.args:
+                if arg.id in self.registers:
+                    circuit.add_register(self.registers[arg.id])
+            self.circuits[var_name] = circuit
+
+    # Obtener los parámetros del registro
+    def _get_register_params(self, node):
+        # Definir los parámetros del registro
+        register_params = {
+            'size': 0,
+            'name': None,
+            'bits': None
+        }
+        # Argumentos sin etiqueta
+        for i, arg in enumerate(node.value.args):
+            if i == 0:
+                register_params['size'] = arg.value
+            elif i == 1:
+                register_params['name'] = arg.value
+            elif i == 2:
+                register_params['bits'] = arg.value
+        # Argumentos con etiqueta
+        for keyword in node.value.keywords:
+            register_params[keyword.arg] = keyword.value.value
+
+        return register_params
+
+    # Establecer el registro (cuántico o clásico)
+    def _set_register(self, var_name, node):
+        # Si el registro no ha sido creado anteriormente
+        if var_name not in self.registers:
+            # Obtener los parámetros del registro
+            register_params = self._get_register_params(self, node)
+            # Crear y almacenar los registros
+            if node.value.func.id == 'QuantumRegister':
+                self.registers[var_name] = QuantumRegister(
+                    size=register_params['size'], 
+                    name=register_params['name'], 
+                    bits=register_params['bits'])
+            else:
+                self.registers[var_name] = ClassicalRegister(
+                    size=register_params['size'], 
+                    name=register_params['name'], 
+                    bits=register_params['bits'])
+
     def visit_Assign(self, node):
         # Asignaciones de variables con funciones o clases
         if isinstance(node.value, ast.Call) and isinstance(node.value.func, ast.Name):
             var_name = node.targets[0].id
             # Verifica si es una asignación de QuantumCircuit, QuantumRegister, ClassicalRegister u otro
             if node.value.func.id == 'QuantumCircuit':
-                # Si el circuito no ha sido creado anteriormente
-                if var_name not in self.circuits:
-                    circuit = QuantumCircuit()
-                    # Añadir cada registro asignado al circuito
-                    for arg in node.value.args:
-                        if arg.id in self.registers:
-                            circuit.add_register(self.registers[arg.id])
-                    self.circuits[var_name] = circuit
+                # Establecer el circuito cuántico
+                self._set_quantum_circuit(self, var_name, node)
             elif node.value.func.id in ['QuantumRegister', 'ClassicalRegister']:
-                # Si el registro no ha sido creado anteriormente
-                if var_name not in self.registers:
-                    # Definir los parámetros del registro
-                    register_params = {
-                        'size': 0,
-                        'name': None,
-                        'bits': None
-                    }
-                    # Obtener los parámetros del registro
-                    # # Argumentos sin etiqueta
-                    for i, arg in enumerate(node.value.args):
-                        if i == 0:
-                            register_params['size'] = arg.value
-                        elif i == 1:
-                            register_params['name'] = arg.value
-                        elif i == 2:
-                            register_params['bits'] = arg.value
-                    # # Argumentos con etiqueta
-                    for keyword in node.value.keywords:
-                        register_params[keyword.arg] = keyword.value.value
-                    # Crear y almacenar los registros
-                    if node.value.func.id == 'QuantumRegister':
-                        self.registers[var_name] = QuantumRegister(
-                            size=register_params['size'], 
-                            name=register_params['name'], 
-                            bits=register_params['bits'])
-                    else:
-                        self.registers[var_name] = ClassicalRegister(
-                            size=register_params['size'], 
-                            name=register_params['name'], 
-                            bits=register_params['bits'])
+                # Establecer el registro (cuántico o clásico)
+                self._set_register(self, var_name, node)
             else:
                 if var_name not in self.code_vars:
                     self.code_vars[var_name] = {}
@@ -87,6 +103,40 @@ class QiskitAnalyzer(ast.NodeVisitor):
 
         self.generic_visit(node)
 
+    # Obtener los argumentos de la instrucción
+    def _get_instruction_args(self, node, circuit_instruction_name):
+        # Definir los argumentos de la instrucción
+        instruction_args = {
+            'name': circuit_instruction_name,
+            'num_qubits': 0,
+            'num_clbits': 0,
+            'params': []
+        }
+        qargs = []
+        cargs = []
+
+        # Obtener los argumentos de la instrucción
+        for arg in node.value.args:
+            # Si se referencia un QuantumRegister
+            if isinstance(arg, ast.Subscript):
+                print(arg)
+                # Obtener la variable y la posicion del registro
+                register_var = arg.value.id
+                register_pos = arg.slice.value
+                print(f"{register_var}[{register_pos}]")
+                # Obtener la variable y el index
+                register = self.registers[register_var][register_pos]
+                print(register)
+                # Almacenar el registro en los argumentos de la instrucción
+                if isinstance(register, Qubit):
+                    instruction_args['num_qubits'] += 1
+                    qargs.append(register)
+                elif isinstance(register, Clbit):
+                    instruction_args['num_clbits'] += 1
+                    cargs.append(register)
+
+        return instruction_args, qargs, cargs
+
     def visit_Expr(self, node):
         # Verificar si es una instrucción aplicada a un circuito cuántico
         if isinstance(node.value, ast.Call) and isinstance(node.value.func, ast.Attribute):
@@ -94,31 +144,8 @@ class QiskitAnalyzer(ast.NodeVisitor):
             if circuit_name in self.circuits:
                 circuit_instruction_name = node.value.func.attr
                 print("\nCircuito", circuit_name, ": Instrucción", circuit_instruction_name)
-                instruction_args = {
-                    'name': circuit_instruction_name,
-                    'num_qubits': 0,
-                    'num_clbits': 0,
-                    'params': []
-                }
-                qargs = []
-                cargs = []
-                for arg in node.value.args:
-                    if isinstance(arg, ast.Subscript): # Referencia a un QuantumRegister
-                        print(arg)
-                        # Obtener la variable y la posicion del registro
-                        register_var = arg.value.id
-                        register_pos = arg.slice.value
-                        print(f"{register_var}[{register_pos}]")
-                        # Obtener la variable y el index
-                        register = self.registers[register_var][register_pos]
-                        print(register)
-                        # Almacenar el registro en los argumentos de la instrucción
-                        if isinstance(register, Qubit):
-                            instruction_args['num_qubits'] += 1
-                            qargs.append(register)
-                        elif isinstance(register, Clbit):
-                            instruction_args['num_clbits'] += 1
-                            cargs.append(register)
+                # Obtener los argumentos de la instrucción
+                instruction_args, qargs, cargs = self._get_instruction_args(self, node, circuit_instruction_name)
                 # Aplicar instrucción al circuito
                 self.circuits[circuit_name].append(Instruction(**instruction_args), qargs, cargs)
 
