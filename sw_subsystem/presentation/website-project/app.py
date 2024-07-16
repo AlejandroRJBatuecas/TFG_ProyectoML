44f8d4b46a79b2d88c438d87970695b544d4b383
AlejandroRJB <alejandroramiro.jimenez@alu.uclm.es>
2024-07-16 18:26:56 +0200

    Creación de las rutas y páginas básicas e implementación de la página de inicio

--- sw_subsystem/presentation/website-project/app.py
+++ sw_subsystem/presentation/website-project/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hola Mundo'
+def index():
+    return render_template('index.html')
+
+@app.route('/analisis_patrones')
+def pattern_analysis():
+    return render_template('/pattern_analysis/pattern_analysis.html')
+
+@app.route('/modelos_ml')
+def ml_models():
+    return render_template('/ml_models/ml_models.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
